3fc5f3c415cc42cc8d736c74e51fd9aa9a8b6e75
Dario <dariohdez16@gmail.com>
2018-09-26 21:20:44 +0200

    solo el script

--- Datos.py
+++ Datos.py
@@ -6,43 +6,58 @@ class Datos(object):
     tipoAtributos = []
     nombreAtributos = []
     nominalAtributos = []
-    datos = np.array([],dtype = np.int32)
+    datos = np.array([],dtype=np.int32)
     # Lista de diccionarios. Uno por cada atributo.
     diccionarios = []
-    print ("hola")
-    # TODO: procesar el fichero para asignar correctamente las variables tipoAtributos, nombreAtributos, nominalAtributos, datos y diccionarios
+    # TODO: procesar el fichero para asignar correctamente las variables
+    # tipoAtributos, nombreAtributos, nominalAtributos, datos y diccionarios
     def __init__(self, nombreFichero):
         fichero = open(nombreFichero, 'r')
 
         nDatos = int(fichero.readline())
-        nombreAtributos = fichero.readline().strip().split(",")
-        tipoAtributos = fichero.readline().strip().split(",")
-        print(nombreAtributos)
-        print(tipoAtributos)
+        self.nombreAtributos = fichero.readline().strip().split(",")
+        self.tipoAtributos = fichero.readline().strip().split(",")
+
         for atributo in Datos.tipoAtributos:
             if atributo == 'Continuo':Datos.nominalAtributos.append(False)
             elif atributo == 'Nominal':Datos.nominalAtributos.append(True)
             else:
                 raise (ValueError)
 
-        datos = [fichero.readline().split('\n')[0].split(',') for i in range(nDatos)]
-        print(datos)
+        contador = [0]*(len(self.tipoAtributos))
+        fila = [0]*(len(self.tipoAtributos))
+
+        self.diccionarios = [{} for i in  range(len(self.tipoAtributos))]
+
+        for i in range(nDatos):
+            linea = fichero.readline().split('\n')[0].split(",")  # linea = [data,data,data....]
+            for j in range(len(linea)):
+                if self.tipoAtributos[j] == 'Nominal':
+                    if not linea[j] in self.diccionarios[j]:
+                        self.diccionarios[j].update({linea[j]:contador[j]})
+                        contador[j] += 1
+
+                    fila[j] = self.diccionarios[j].get(linea[j])
+                else:
+                    fila[j] = linea[j]
+
+            self.datos = np.append(self.datos)
+
+
+
+        print(self.diccionarios)
+        #self.datos = [fichero.readline().split('\n')[0].split(',') for i in range(nDatos)]
+
         #diccionario
+        #for i in range(len(self.tipoAtributos)):
 
 
 
 
-
-
-
-
-
-
-        pass
     def extraeDatos(self,idx):
 
         pass
-dataset = Datos('tic-tac-toe.data')
+dataset = Datos('balloons.data')
 
 print(dataset.datos)
 
