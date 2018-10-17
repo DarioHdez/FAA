7df9ba7cbf226bf2c5c2cea2ef7bbd31f3dce6ad
Tu Nombre <you@example.com>
2018-10-17 21:35:09 +0200

    NB

--- practica1/EstrategiaParticionado.py
+++ practica1/EstrategiaParticionado.py
@@ -2,6 +2,7 @@
 from abc import ABCMeta,abstractmethod
 import random
 import Datos
+import numpy as np
 
 class Particion():
 
@@ -20,7 +21,6 @@ class EstrategiaParticionado(object):
   # Atributos: deben rellenarse adecuadamente para cada estrategia concreta: nombreEstrategia, numeroParticiones, listaParticiones. Se pasan en el constructor
 
   @abstractmethod
-  # TODO: esta funcion deben ser implementadas en cada estrategia concreta
   def creaParticiones(self,datos,seed=None):
     pass
 
@@ -34,17 +34,14 @@ class ValidacionSimple(EstrategiaParticionado):
   def creaParticiones(self,datos,seed=None):
     p = Particion()
     num = datos.shape[0]/2
-
     random.seed(seed)
 
     while len(p.indicesTrain) < num:
         a =random.randint(0,datos.shape[0]-1)
-
         if not a in p.indicesTrain: p.indicesTrain.append(a)
+    #p.indicesTrain = list({random.randint(0,datos.shape[0]-1) for n in range(num)})
 
-    for i in range(datos.shape[0]):
-        if not i in p.indicesTrain:
-            p.indicesTest.append(i)
+    p.indicesTest = [n for n in range(datos.shape[0]) if n not in p.indicesTrain]
 
     return p
 
@@ -54,15 +51,20 @@ class ValidacionCruzada(EstrategiaParticionado):
   # Crea particiones segun el metodo de validacion cruzada.
   # El conjunto de entrenamiento se crea con las nfolds-1 particiones y el de test con la particion restante
   # Esta funcion devuelve una lista de particiones (clase Particion)
-  # TODO: implementar
   def creaParticiones(self,datos,seed=None):
     p= Particion()
-    num = datos.shape[0]/3
+    num = datos.shape[0]/3 # check this
     random.seed(seed)
 
-    np.random.shuffle(datos)
-    
-    #while len
+    #Vamos a trabajar con los indices directamente
+    lista=datos.tolist()
+    listaInd = [n for n in range(len(lista))]
+    random.shuffle(listaInd)
+
+    p.indicesTrain = [ n for n in listaInd if listaInd.index(n) <= num*2]
+    p.indicesTest = [ n for n in listaInd if n not in p.indicesTrain]
+
+    return p
 
 
 #####################################################################################################
@@ -70,7 +72,12 @@ class ValidacionBootstrap(EstrategiaParticionado):
 
   # Crea particiones segun el metodo de validacion por bootstrap.
   # Esta funcion devuelve una lista de particiones (clase Particion)
-  # TODO: implementar
   def creaParticiones(self,datos,seed=None):
+    p = Particion()
+    num = datos.shape[0]
     random.seed(seed)
-    pass
+
+    p.indicesTrain = [random.randint(0,datos.shape[0]-1) for n in range(num)]
+    p.indicesTest = [n for n in range(datos.shape[0]) if n not in p.indicesTrain]
+
+    return p
