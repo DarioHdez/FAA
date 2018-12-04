42959ec291d1a25dbd327cc763bf22d939710a9a
Mrmarthos <43417351+Mrmarthos@users.noreply.github.com>
2018-12-04 20:55:26 +0100

    Comienzo

--- practica3/AlgoritmosGeneticos.py
+++ practica3/AlgoritmosGeneticos.py
@@ -1,5 +1,50 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 from Estrategias.ValidacionCruzada import ValidacionCruzada
-from copy import deepcopy
 
+from Clasificador import Clasificador
+from random import randint
+import numpy as np
+
+#########################################################################
+
+class ClasificadorAG(Clasificador):
+
+
+
+    def __init__(self, K=1):
+        super().__init__()
+
+
+
+
+
+    def entrenamiento(self, datostrain, atributosDiscretos=None, diccionario=None, tampoblacion=100,generaciones=100,num_reglas=10,k=10):
+
+        individuos = genera_individuos(tampoblacion, num_reglas,  atributosDiscretos, k)
+
+        #calcular fitness
+        #recombinar
+        #mutacion
+        #supervivientes
+        pass
+
+    def clasifica(self, datostest, atributosDiscretos=None, diccionario=None):
+        pass
+
+    def genera_individuos(tampoblacion, num_reglas,  atributosDiscretos, k):
+
+        individuos=[]
+
+        for i in range(tampoblacion):
+            individuo = []
+            num_reglas = randint(1,num_reglas)
+            for z in range(num_reglas):
+                regla = [randint(0, k) for _ in range(len(atributosDiscretos) - 1)] + [randint(0, 1)]
+                individuo.append(regla)
+
+            individuos.append(individuo)
+
+        return individuos
+
+#########################################################################
