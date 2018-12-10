5357b508fdbda59c5b459123cc5bea5a162e2b72
Mrmarthos <43417351+Mrmarthos@users.noreply.github.com>
2018-12-10 21:28:21 +0100

    Fitness

--- practica3/Clasificadores/ClasificadorAlgoritmosGeneticos.py
+++ practica3/Clasificadores/ClasificadorAlgoritmosGeneticos.py
@@ -15,6 +15,7 @@ class ClasificadorAG(Clasificador):
         super().__init__()
 
 
+
     def _genera_individuos(tampoblacion, num_reglas,  atributosDiscretos, k):
 
         individuos=[]
@@ -32,10 +33,13 @@ class ClasificadorAG(Clasificador):
 
 
     def entrenamiento(self, datostrain, atributosDiscretos=None, diccionario=None, tampoblacion=100,generaciones=100,num_reglas=10,k=10):
-
+        
         individuos = self._genera_individuos(tampoblacion, num_reglas,  atributosDiscretos, k)
 
         #calcular fitness
+        for i in range(generaciones):
+            fitness = fitness(datostrain, individuos)
+            self.update_stats(individuos, fitness)
         #recombinar
         #mutacion
         #supervivientes
@@ -44,6 +48,35 @@ class ClasificadorAG(Clasificador):
     def clasifica(self, datostest, atributosDiscretos=None, diccionario=None):
         pass
 
+    def fitness(self, datostrain, individuos):
+        aciertos = 0
+        datosdiscretizados = self.discretizar_elementos(datostrain)
+
+        for dato in datosdiscretizados:
+            for individuo in individuos:
+                for regla in individuo:
+                    if ((dato == regla).all() or ((dato[:-1] != regla[:-1]).any() and dato[-1] != regla[-1])):
+                        flag = True
+                        break
+                if flag:
+                    aciertos += 1
 
 
+
+        return aciertos * 1. / len(datostrain)
+
+    def discretizar_elementos(self, datostrain):
+        datos_discretos = np.copy(datostrain)
+        k=ceil(1+ 3.322*log10(len(datostrain)))
+
+        for i in range(datostrain.shape[1]-1):
+            xmin = min(datostrain[:, i])
+            xmax = max(datostrain[:, i])
+            a = np.around((xmax - xmin) / k, decimals=3)
+
+            for j in range(datostrain.shape[0]):
+                intervalo = datostrain[j, i] // a
+                datos_discretos[j, i] = np.clip(intervalo, 1, k)
+
+        return datos_discretos
 #########################################################################
